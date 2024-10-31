a0e7972e6b45cdae1ef1c2323d6a6136c49ca87f
saadetsimsek <saadetsimsek13@gmail.com>
2024-10-31 13:11:16 +0300

    Initial Commit

---
+++ AutocompleteTextfieldTests/AutocompleteTextfieldTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutocompleteTextfieldTests.swift
+//  AutocompleteTextfieldTests
+//
+//  Created by Saadet Şimşek on 31/10/2024.
+//
+
+import Testing
+@testable import AutocompleteTextfield
+
+struct AutocompleteTextfieldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
